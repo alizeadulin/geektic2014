d035d4b5dbfd70d0e28c6c62b9ce8ed3079c8f9e
alize <alizeadulin@gmail.com>
2014-06-25 10:29:24 +0200

    modification css, modif controller app.js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -4,18 +4,19 @@ app.config(function($routeProvider){
 	$routeProvider.when("/", {
 		controller: 'MainCtrl',
 		templateUrl: 'formulaireRecherche.html'
-	}).when("/geek", {
-		controller: 'SrcCtrl',
+	}).when("/geek/:sexe/:interets", {
 		templateUrl: 'resultatRecherche.html'
 	})
 	})
 
 app.controller('MainCtrl', function($scope, $http) {
-	  $scope.users = [{name:'Aliz'},{name:'Alex'}];
-	  $http.get('http://23.251.141.246/users').
-	    success(function(data){ $scope.users = data; });
-		
 	}); 
+
+/*app.controller('SrcCtrl', function($scope, $http) {
+	$http.get('/geek/:sexe/:interets').success(function(getListSelectedGeek) {
+	  $scope.geeks = getListSelectedGeek;
+		
+	}); */
 
 /*var app = angular.module("geektic", ['ngRoute']);
 
